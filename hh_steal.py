c40b6d99e88cb7e2ada825649787b73417486e04
Exord <wecatorz@gmail.com>
2020-01-13 01:28:26 +0300

    ResultToDataframe

--- hh_steal.py
+++ hh_steal.py
@@ -1,13 +1,15 @@
 from http.cookiejar import request_host
 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup as bs
+
 
 #emulaciya povedeniya brauzera
 headers = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
 
-base_Url = 'https://pushkino.hh.ru/search/vacancy?resume=f45bd12eff079396740039ed1f353133794170&from=resumelist'
+base_Url = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=Junior+python+developer&from=suggest_post'
 
 def hh_Parse(base_url, headers):
     session = requests.Session();
@@ -17,11 +19,23 @@ def hh_Parse(base_url, headers):
         soup = bs(request.content, 'html.parser')
         divs = soup.find_all('div', attrs = {'data-qa': 'vacancy-serp__vacancy'})
 
+        colum_Names = ['name_Vacancy', 'name_Company', 'vacancy_Link', 'vacancy_Payday']
+
+        vacancy_DF = pd.DataFrame(columns = colum_Names)
+
         for div in divs:
             name_Vacancy = div.find('a', attrs = {'data-qa': 'vacancy-serp__vacancy-title'}).text
+            name_Company = div.find('a', attrs = {'data-qa': 'vacancy-serp__vacancy-employer'}).text
             link = div.find('a', attrs = {'data-qa': 'vacancy-serp__vacancy-title'})['href']
-            print("{0} {1}".format(name_Vacancy, link))
-        #print(divs)
+            try:
+                payday = div.find('div', attrs = {'class' : 'vacancy-serp-item__compensation'}).text
+            except AttributeError:
+                payday = div.find('div', attrs={'class': 'vacancy-serp-item__compensation'})
+
+            vacancy_DF.loc[len(vacancy_DF)] = [name_Vacancy, name_Company, link, payday]
+            #print("{} | {} | {} | {}".format(name_Vacancy, payday, name_Company, link))
+
+        print(vacancy_DF)
     else:
         print('ERROR')
 
